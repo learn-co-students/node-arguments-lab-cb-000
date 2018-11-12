d31d82a6845d3b9c5bade7fdcc462995a1aa9bbb
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-11-12 14:59:12 +0000

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,20 @@
-// code solution here
+function generatePassword(length){
+  var pass = ''
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for(let i = 0; i < length; i++){
+    pass +=possible.charAt(Math.floor(Math.random()*possible.length))
+  }
+
+  console.log(pass)
+  return pass
+}
+
+if(process.argv.length!==4){
+    console.log('Need two arguments')
+    return
+}
+else{
+  for(let i = 0; i < parseInt(process.argv[3]);i++)
+  generatePassword(process.argv[2])
+}
