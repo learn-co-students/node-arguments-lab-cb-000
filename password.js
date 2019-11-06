c4375b9f1049cdfd92ae829c37c3a70c3959c09d
dapawn <dapawn@gmail.com>
2019-11-06 08:57:29 -0500

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,15 @@
 // code solution here
+function generatePassword(length) {
+    var length = length || 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; i++) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+for (var i=1; i<= process.argv[3]; i++) {
+  console.log( generatePassword(process.argv[2]));
+}
+ process.exit(0);
